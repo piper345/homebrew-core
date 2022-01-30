7a6d62dc9c98b5f6ba29e38ee46137a44d895822
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-30 05:33:13 +0000

    swagger-codegen@2 3.0.32

--- Formula/swagger-codegen@2.rb
+++ Formula/swagger-codegen@2.rb
@@ -1,8 +1,8 @@
 class SwaggerCodegenAT2 < Formula
   desc "Generate clients, server stubs, and docs from an OpenAPI spec"
   homepage "https://swagger.io/swagger-codegen/"
-  url "https://github.com/swagger-api/swagger-codegen/archive/v2.4.25.tar.gz"
-  sha256 "bec77e14811ebc38c0f65eafd62681b2475c55cef62c900a25aef16f4a339a92"
+  url "https://github.com/swagger-api/swagger-codegen/archive/v3.0.32.tar.gz"
+  sha256 "bcabdd1047534a7b4ea5b842f17c7cb09b76b31f672d1db3014315b4d300eb7e"
   license "Apache-2.0"
 
   livecheck do
