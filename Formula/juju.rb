21c4f22c641ddcc2b4bab660b3786e090c7edf75
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-12 05:17:54 +0000

    juju 2.9.24

--- Formula/juju.rb
+++ Formula/juju.rb
@@ -2,8 +2,8 @@ class Juju < Formula
   desc "DevOps management tool"
   homepage "https://juju.is/"
   url "https://github.com/juju/juju.git",
-      tag:      "juju-2.9.23",
-      revision: "b9b2dc63924d8c503fa6b6c108168905e9466ca3"
+      tag:      "juju-2.9.24",
+      revision: "f1924dcf59c89c72dbce91d6d5f37bb376e8660a"
   license "AGPL-3.0-only"
   version_scheme 1
   head "https://github.com/juju/juju.git", branch: "develop"
