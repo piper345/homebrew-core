89af7cec9e53f858508f050fabf0ec85c5d83296
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-02-15 13:57:37 +0800

    meson: remove `inreplace`
    This inreplace is no longer valid. We'll need to check what this needs
    to be changed to when the build completes.

--- Formula/meson.rb
+++ Formula/meson.rb
@@ -16,21 +16,6 @@ class Meson < Formula
   def install
     python3 = Formula["python@3.10"].opt_bin/"python3"
     system python3, *Language::Python.setup_install_args(prefix)
-
-    # Make the bottles uniform. This also ensures meson checks `HOMEBREW_PREFIX`
-    # for fulfilling dependencies rather than just `/usr/local`.
-    mesonbuild = prefix/Language::Python.site_packages(python3)/"mesonbuild"
-    inreplace_files = %w[
-      coredata.py
-      dependencies/boost.py
-      dependencies/cuda.py
-      dependencies/qt.py
-      mesonlib/universal.py
-      modules/python.py
-    ].map { |f| mesonbuild/f }
-
-    # Passing `build.stable?` ensures a failed `inreplace` won't fail HEAD installs.
-    inreplace inreplace_files, "/usr/local", HOMEBREW_PREFIX, build.stable?
   end
 
   test do
