bbaa623c4a7b0237f1dcc5638bcac163962d1a49
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-23 05:08:47 +0000

    cromwell 77

--- Formula/cromwell.rb
+++ Formula/cromwell.rb
@@ -1,8 +1,8 @@
 class Cromwell < Formula
   desc "Workflow Execution Engine using Workflow Description Language"
   homepage "https://github.com/broadinstitute/cromwell"
-  url "https://github.com/broadinstitute/cromwell/releases/download/76/cromwell-76.jar"
-  sha256 "0c9fea35ddb8d6a4a5751d08de90da87c8a9fc6128d3861843555bdfcf4a7e84"
+  url "https://github.com/broadinstitute/cromwell/releases/download/77/cromwell-77.jar"
+  sha256 "0823aee9257c6a1d06eabd53ef1bfae6cdf398a27fd70fe07d7630b32b74666a"
   license "BSD-3-Clause"
 
   bottle do
