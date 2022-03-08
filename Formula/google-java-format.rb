c5b918f8e67460df865c4d91d8443d1195146b98
Michael Cho <cho-m@tuta.io>
2022-03-07 21:39:42 -0800

    google-java-format: update resource and remove exports

--- Formula/google-java-format.rb
+++ Formula/google-java-format.rb
@@ -15,18 +15,13 @@ class GoogleJavaFormat < Formula
   depends_on "python@3.10"
 
   resource "google-java-format-diff" do
-    url "https://raw.githubusercontent.com/google/google-java-format/v1.14.0/scripts/google-java-format-diff.py"
+    url "https://raw.githubusercontent.com/google/google-java-format/v1.15.0/scripts/google-java-format-diff.py"
     sha256 "4c46a4ed6c39c2f7cbf2bc7755eefd7eaeb0a3db740ed1386053df822f15782b"
   end
 
   def install
     libexec.install "google-java-format-#{version}-all-deps.jar" => "google-java-format.jar"
-    bin.write_jar_script libexec / "google-java-format.jar", "google-java-format",
-      "--add-exports jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED \
-      --add-exports jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED \
-      --add-exports jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED \
-      --add-exports jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED \
-      --add-exports jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED"
+    bin.write_jar_script libexec/"google-java-format.jar", "google-java-format"
     resource("google-java-format-diff").stage do
       bin.install "google-java-format-diff.py" => "google-java-format-diff"
       rewrite_shebang detected_python_shebang, bin/"google-java-format-diff"
