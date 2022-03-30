bd4f52530903e4ad6f3e85c0d54a1211fdafd287
Michael Cho <cho-m@tuta.io>
2022-03-30 00:52:46 -0700

    nexus: fix url and sha256

--- Formula/nexus.rb
+++ Formula/nexus.rb
@@ -1,8 +1,8 @@
 class Nexus < Formula
   desc "Repository manager for binary software components"
   homepage "https://www.sonatype.org/"
-  url "https://github.com/sonatype/nexus-public/archive/release-3.38.1-01.tar.gz"
-  sha256 "7ebf288cea60ef785ccca74336726905c75e4a88c45aac02e65a6b3dbbf07002"
+  url "https://github.com/sonatype/nexus-public/archive/refs/tags/release-3.38.1-01.tar.gz"
+  sha256 "83b3a39e4d350d9786ce47410607fdd9ec04fca4f8451c0a763d8e22c5639e87"
   license "EPL-1.0"
 
   # As of writing, upstream is publishing both v2 and v3 releases. The "latest"
