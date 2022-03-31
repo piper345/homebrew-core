66c7e64928b66610e49e869fcd818d9420c42666
Rui Chen <rui@chenrui.dev>
2022-03-31 01:31:56 -0400

    update test
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/flarectl.rb
+++ Formula/flarectl.rb
@@ -23,6 +23,6 @@ class Flarectl < Formula
 
   test do
     ENV["CF_API_TOKEN"] = "invalid"
-    assert_match "HTTP status 400: Invalid request headers (6003)", shell_output("#{bin}/flarectl u i", 1)
+    assert_match "Invalid request headers (6003)", shell_output("#{bin}/flarectl u i", 1)
   end
 end
