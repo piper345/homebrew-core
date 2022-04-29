54a107643b9c5de7696183f0acf45575b9b6d3fa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-29 05:46:38 +0000

    trino 379

--- Formula/trino.rb
+++ Formula/trino.rb
@@ -3,8 +3,8 @@ class Trino < Formula
 
   desc "Distributed SQL query engine for big data"
   homepage "https://trino.io"
-  url "https://search.maven.org/remotecontent?filepath=io/trino/trino-server/375/trino-server-375.tar.gz"
-  sha256 "293c1456f1cb4292890226cc092884ab85cd206e52704509e2e27a58d3b318b6"
+  url "https://search.maven.org/remotecontent?filepath=io/trino/trino-server/379/trino-server-379.tar.gz"
+  sha256 "78eb55529f8c2e20b8e487096b857251d024b623cef122eb29e5be3aebd1caf4"
   license "Apache-2.0"
 
   livecheck do
