c6f3dd35939bb3de0b55d77926351747e667fd35
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-19 05:20:38 +0000

    gmic 3.1.2

--- Formula/gmic.rb
+++ Formula/gmic.rb
@@ -1,8 +1,8 @@
 class Gmic < Formula
   desc "Full-Featured Open-Source Framework for Image Processing"
   homepage "https://gmic.eu/"
-  url "https://gmic.eu/files/source/gmic_3.0.1.tar.gz"
-  sha256 "6cc20a20e3ab53ce485ccf6e044a30141b3d62cf7743b83bb04906ff29453035"
+  url "https://gmic.eu/files/source/gmic_3.1.2.tar.gz"
+  sha256 "d5070c559c75161a225d74a99c19faf2485a3983139290f998cc6359bb318ba4"
   license "CECILL-2.1"
   head "https://github.com/dtschump/gmic.git", branch: "master"
 
