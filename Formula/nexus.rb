2fbcbe79cce0c6e4297d664ea552bda9166e2b1c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-28 05:40:42 +0000

    nexus 3.40.1-01

--- Formula/nexus.rb
+++ Formula/nexus.rb
@@ -1,8 +1,8 @@
 class Nexus < Formula
   desc "Repository manager for binary software components"
   homepage "https://www.sonatype.org/"
-  url "https://github.com/sonatype/nexus-public/archive/refs/tags/release-3.38.1-01.tar.gz"
-  sha256 "83b3a39e4d350d9786ce47410607fdd9ec04fca4f8451c0a763d8e22c5639e87"
+  url "https://github.com/sonatype/nexus-public/archive/refs/tags/release-3.40.1-01.tar.gz"
+  sha256 "ec11b10a4f3becc4a7932ffed63f0ce7e7d1451e69bbf5b74623326f47bf7db8"
   license "EPL-1.0"
 
   # As of writing, upstream is publishing both v2 and v3 releases. The "latest"
