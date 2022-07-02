2fe2693d879327f9166c07a9d42b1c62a440148e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-02 05:34:23 +0000

    sonobuoy 0.56.8

--- Formula/sonobuoy.rb
+++ Formula/sonobuoy.rb
@@ -1,8 +1,8 @@
 class Sonobuoy < Formula
   desc "Kubernetes component that generates reports on cluster conformance"
   homepage "https://github.com/vmware-tanzu/sonobuoy"
-  url "https://github.com/vmware-tanzu/sonobuoy/archive/v0.56.7.tar.gz"
-  sha256 "664746d50fec283e16185c3463acdec04a987d65ccee75d4a0a5d3c08c50e708"
+  url "https://github.com/vmware-tanzu/sonobuoy/archive/v0.56.8.tar.gz"
+  sha256 "8e2631c57c2fd7ffc811c1481ab9df15043770b47cec3fafa15d1c64b8f13326"
   license "Apache-2.0"
 
   bottle do
