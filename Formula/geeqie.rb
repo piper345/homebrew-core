1ad00a65b1b5c35fcc36b1f814880e7c8285c330
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-13 05:16:09 +0000

    geeqie 2.0.1

--- Formula/geeqie.rb
+++ Formula/geeqie.rb
@@ -1,10 +1,9 @@
 class Geeqie < Formula
   desc "Lightweight Gtk+ based image viewer"
   homepage "https://www.geeqie.org/"
-  url "https://github.com/BestImageViewer/geeqie/releases/download/v1.7.3/geeqie-1.7.3.tar.xz"
-  sha256 "25b1f71cf91bd9a96f399d2a9e70507e54bb377a56e64d89521c0f7a9ce5dd38"
+  url "https://github.com/BestImageViewer/geeqie/releases/download/v2.0.1/geeqie-2.0.1.tar.xz"
+  sha256 "89c1a7574cfe3888972d10723f4cf3a277249bea494fd9c630aa8d0df944555d"
   license "GPL-2.0-or-later"
-  revision 1
 
   livecheck do
     url :stable
