9e60bf55d584da1fed302459b0547de2070e7ad0
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-08-26 01:28:14 +0800

    emscripten: update resources

--- Formula/emscripten.rb
+++ Formula/emscripten.rb
@@ -53,7 +53,7 @@ class Emscripten < Formula
   # See llvm resource below for instructions on how to update this.
   resource "binaryen" do
     url "https://github.com/WebAssembly/binaryen.git",
-        revision: "62924de6959bb6fe28c1dcb5b788104e05d5467f"
+        revision: "594ff7b9609656edb83187cb4600b23b3f2fde37"
   end
 
   # emscripten needs argument '-fignore-exceptions', which is only available in llvm >= 12
@@ -64,7 +64,7 @@ class Emscripten < Formula
   # Then use the listed llvm_project_revision for the resource below.
   resource "llvm" do
     url "https://github.com/llvm/llvm-project.git",
-        revision: "941959d69de76342fbeebcebd9f0ebdf2f73c77d"
+        revision: "75767a0f9a926641edbef08e31ec2148ff45da67"
   end
 
   def install
