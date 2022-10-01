b49612cac1a2d3b85794e1565d4b7c323287218a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-01 05:52:55 +0000

    teleport 10.3.1

--- Formula/teleport.rb
+++ Formula/teleport.rb
@@ -1,8 +1,8 @@
 class Teleport < Formula
   desc "Modern SSH server for teams managing distributed infrastructure"
   homepage "https://gravitational.com/teleport"
-  url "https://github.com/gravitational/teleport/archive/v10.2.6.tar.gz"
-  sha256 "85090cbc25ff0d415488c5dedfe2f4c40d1b72b8ee5c61eda11ee14b71bce314"
+  url "https://github.com/gravitational/teleport/archive/v10.3.1.tar.gz"
+  sha256 "1546a7ad535732fd6d6e80d54bca31b6f02195cf5f58b6930801a1b457d47516"
   license "Apache-2.0"
   head "https://github.com/gravitational/teleport.git", branch: "master"
 
