55fd4dd9c75c8e5efc09b0b16c8c7e5da7079f3a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-12 05:24:29 +0000

    convox 3.6.6

--- Formula/convox.rb
+++ Formula/convox.rb
@@ -1,8 +1,8 @@
 class Convox < Formula
   desc "Command-line interface for the Convox PaaS"
   homepage "https://convox.com/"
-  url "https://github.com/convox/convox/archive/3.5.6.tar.gz"
-  sha256 "4713f8a4838c95023915fa371ea6e524e78ef34793d96034b1a702ee99136dcc"
+  url "https://github.com/convox/convox/archive/3.6.6.tar.gz"
+  sha256 "56ad547b699d00cbfb9ace71784351bec1cdcaf376e7b01f043d0bdae37176a6"
   license "Apache-2.0"
   version_scheme 1
 
