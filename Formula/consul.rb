ad0dcfda83d4bdd01d20ce14f6cbf95778f3eeb3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-12-01 05:06:12 +0000

    consul 1.14.2

--- Formula/consul.rb
+++ Formula/consul.rb
@@ -1,8 +1,8 @@
 class Consul < Formula
   desc "Tool for service discovery, monitoring and configuration"
   homepage "https://www.consul.io"
-  url "https://github.com/hashicorp/consul/archive/refs/tags/v1.13.3.tar.gz"
-  sha256 "bdc3dc7e4d5d4448528e9bf6fee1cb3b613a0f730820988d4fc550e189bfd46c"
+  url "https://github.com/hashicorp/consul/archive/refs/tags/v1.14.2.tar.gz"
+  sha256 "9cde03a6d7c466afd0a54167108a616389b9bdac752eecf9b5fb40f001960854"
   license "MPL-2.0"
   head "https://github.com/hashicorp/consul.git", branch: "main"
 
