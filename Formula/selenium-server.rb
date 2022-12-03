4bcd6b80fa12c799ebf7d6fa29ddd069dced363f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-12-03 05:27:04 +0000

    selenium-server 4.7.0

--- Formula/selenium-server.rb
+++ Formula/selenium-server.rb
@@ -1,8 +1,8 @@
 class SeleniumServer < Formula
   desc "Browser automation for testing purposes"
   homepage "https://www.selenium.dev/"
-  url "https://github.com/SeleniumHQ/selenium/releases/download/selenium-4.5.0/selenium-server-4.5.0.jar"
-  sha256 "74ed61b1b8c4b6d30449e50be17ecb7b9fd70013373dcfa8447f5c66e3eb53cd"
+  url "https://github.com/SeleniumHQ/selenium/releases/download/selenium-4.7.0/selenium-server-4.7.0.jar"
+  sha256 "79fce69459a48e87d07ac228cbe35789109f4c4c24e5b25b46bcec919d5afe8e"
   license "Apache-2.0"
 
   livecheck do
